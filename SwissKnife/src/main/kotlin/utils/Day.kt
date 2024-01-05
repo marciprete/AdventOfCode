6c803d20bd221ca22fd023d32f98cfc48d6732a7
michele <michele@MBP-di-Lara.lan>
2024-01-05 15:20:37 +0100

    Initial import

--- SwissKnife/src/main/kotlin/utils/Day.kt
+++ SwissKnife/src/main/kotlin/utils/Day.kt
@@ -2,11 +2,31 @@ package it.senape.aoc.utils
 
 import addLeadingZero
 import readInput
+import kotlin.system.measureTimeMillis
 
-abstract class Day(year: Int, day: Int) {
+abstract class Day(private val year: Int, private val day: Int) {
     val input: List<String> = readInput(year, addLeadingZero(day))
     val testInput = readInput(year, addLeadingZero(day)+"_test")
 
-    abstract fun part1(input: List<String>) : Any
-    abstract fun part2(input: List<String>) : Any
+    open fun part1(input: List<String>) : Any? = null
+    open fun part2(input: List<String>) : Any? = null
+
+    companion object {
+        fun solve(day: Day) {
+            with(day) {
+                println("Solving day ${day.day} of $year")
+                measureTimeMillis {
+                    println("Part 1: ${day.part1(input) ?: "Unresolved"}")
+                }.run {
+                    println("Time: $this ms")
+                }
+                measureTimeMillis {
+                    println("Part 2: ${day.part2(input) ?: "Unresolved"}")
+                }.run {
+                    println("Time: $this ms")
+                }
+
+            }
+        }
+    }
 }
---
+++ Year2015/src/main/kotlin/day01/Day01.kt
@@ -0,0 +1,22 @@
+package it.senape.aoc.day01
+
+import it.senape.aoc.utils.Day
+
+class Day01 : Day(2015, 1) {
+    override fun part1(input: List<String>): Any {
+        return input[0].map { 1 - 2 * it.minus('(')}.sum()
+    }
+
+    override fun part2(input: List<String>): Any {
+        var acc = 0
+        return input[0].mapIndexed { idx, c ->
+            when (c) {
+                ')' -> acc -= 1
+                '(' -> acc += 1
+            }
+            if (acc == -1) return idx+1
+        }
+    }
+}
+
+fun main() = Day.solve(Day01())
