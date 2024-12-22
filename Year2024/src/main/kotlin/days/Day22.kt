dd70fa65b7c7b7897afedc114c6f0bc23c6f6c67
michele <michele@MBP-di-Lara.lan>
2024-12-22 14:06:10 +0100

    Day 22 part 1

--- Year2024/src/main/kotlin/days/Day22.kt
+++ Year2024/src/main/kotlin/days/Day22.kt
@@ -5,12 +5,28 @@ import it.senape.aoc.utils.Day
 
 class Day22 : Day(2024, 22) {
     override fun part1(input: List<String>): Any {
-        return 0
+        val inputs = input.map { it.toLong() }
+        val secrets = mutableSetOf<Long>()
+        inputs.forEach { num ->
+            var secret = num
+            for (i in 1..2000) {
+                secret = mixAndPrune(secret * 64, secret)
+                secret = mixAndPrune(secret / 32, secret)
+                secret = mixAndPrune(secret * 2048, secret)
+
+            }
+            secrets.add(secret)
+        }
+        return secrets.sum()
     }
 
     override fun part2(input: List<String>): Any {
         return 0
     }
+
+    fun mixAndPrune(num: Long, secret: Long): Long {
+        return (num xor secret) % 16777216
+    }
 }
 
-fun main() = Day.solve(Day22(), 0, 0)
+fun main() = Day.solve(Day22(), 37327623L, 0)
